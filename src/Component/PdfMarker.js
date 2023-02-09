d936d4e71ec0a272f00bd0d268678394f2100e70
Enes koç <32553624+eneskocc@users.noreply.github.com>
2023-02-09 21:22:41 +0300

    Update PdfMarker.js

--- src/Component/PdfMarker.js
+++ src/Component/PdfMarker.js
@@ -8,9 +8,9 @@ function PdfMarker(props) {
     const [subtitle, setsubtitle] = useState("")
     const [email, setemail] = useState("")
     const [school, setschool] = useState({})
-    const [skill, setSkill] = useState([])
+    const [skill, setSkill] = useState([{ yetenek: "" }])
     const [skillNumber, setSkillNumber] = useState([])
-    const [active, setActive] = useState(2)
+    const [active, setActive] = useState(1)
 
     let items = [];
     for (let number = 1; number <= 5; number++) {
@@ -30,7 +30,7 @@ function PdfMarker(props) {
                     </h1>
                     <Form.Group className="mb-3">
                         <Form.Label htmlFor="TextInput">Name</Form.Label>
-                        <Form.Control id="subtitle" placeholder="Name" value={name} onChange={(e) => setname(e.target.value)} />
+                        <Form.Control id="name" placeholder="Name" value={name} onChange={(e) => setname(e.target.value)} />
                     </Form.Group>
                     <Form.Group className="mb-3">
                         <Form.Label htmlFor="TextInput">Subtitle</Form.Label>
@@ -48,15 +48,15 @@ function PdfMarker(props) {
                     </h1>
                     <Form.Group className="mb-3">
                         <Form.Label htmlFor="TextInput">Yüksek Lisans</Form.Label>
-                        <Form.Control id="subtitle" placeholder="Yüksek Lisans" value={school[0]} onChange={(e) => setschool({...school,yLisnans:e.target.value})} />
+                        <Form.Control id="yLisnans" placeholder="Yüksek Lisans" value={school?.yLisnans} onChange={(e) => setschool({ ...school, yLisnans: e.target.value })} />
                     </Form.Group>
                     <Form.Group className="mb-3">
                         <Form.Label htmlFor="TextInput">Lisans</Form.Label>
-                        <Form.Control id="subtitle" placeholder="Lisans" value={school[1]} onChange={(e) => setschool({...school,lisnans:e.target.value})} />
+                        <Form.Control id="lisnans" placeholder="Lisans" value={school?.lisnans} onChange={(e) => setschool({ ...school, lisnans: e.target.value })} />
                     </Form.Group>
                     <Form.Group className="mb-3">
                         <Form.Label htmlFor="TextInput">Lise</Form.Label>
-                        <Form.Control id="email" placeholder="Lise" value={school[2]} onChange={(e) => setschool({...school,lise:e.target.value})} />
+                        <Form.Control id="lise" placeholder="Lise" value={school?.lise} onChange={(e) => setschool({ ...school, lise: e.target.value })} />
                     </Form.Group>
                 </>;
             case 3:
@@ -84,16 +84,26 @@ function PdfMarker(props) {
                     </h1>
                     <Form.Group className="mb-3">
                         <Form.Label htmlFor="TextInput">Yetenek 1</Form.Label>
-                        <Form.Control id="subtitle" placeholder="Yüksek Lisans" value={school[0]} onChange={(e) => setSkill([...skill, {yetenek:e.target.value}])} />
+                        <Form.Control id={skill[0]?.yetenek} placeholder="Yüksek Lisans" value={skill[0]?.yetenek} onChange={(e) => skill[0].yetenek = e.target.value} />
                     </Form.Group>
                     {skillNumber.map((item, i) => <>
-                    <Form.Group className="mb-3">
-                        <Form.Label htmlFor="TextInput">Yetenek {i+2}</Form.Label>
-                        <Form.Control id="subtitle" placeholder="Yüksek Lisans" value={school[0]} onChange={(e) =>  setSkill([...skill, {yetenek:e.target.value}])} />
-                    </Form.Group>
+                        <Form.Group className="mb-3">
+                            <Form.Label htmlFor="TextInput">Yetenek {i + 2}</Form.Label>
+                            <Form.Control id={skill[i]?.yetenek} placeholder="Yüksek Lisans" value={skill[i + 1]?.yetenek} onChange={(e) => {
+                                console.log( skill[i+1]?.yetenek)
+                                if (skill[i+1]?.yetenek!=undefined) {
+                                    skill[i+1].yetenek = e.target.value;
+                                } else {
+                                    setSkill([...skill, { yetenek: e.target.value }])
+                                }
+
+                                //
+                            }} />
+                        </Form.Group>
                     </>)}
                     <Button variant="danger" onClick={() => {
-                        setSkillNumber([...skillNumber, {number:"e.target.value"}])}}>Add Skill</Button>
+                        setSkillNumber([...skillNumber, { number: "e.target.value" }])
+                    }}>Add Skill</Button>
                 </>;
             case 5:
                 return <h1>
@@ -119,7 +129,7 @@ function PdfMarker(props) {
                     <Pagination size="lg">{items}</Pagination>
                     <br />
                 </div>
-                <Button variant="danger" onClick={() => props.addName(name, subtitle, email,school,skill)}>Save All</Button>
+                <Button variant="danger" onClick={() => props.addName(name, subtitle, email, school, skill)}>Save All</Button>
             </fieldset>
         </Form>
 
