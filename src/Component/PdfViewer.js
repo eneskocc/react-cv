eb0fa6cc5959a2431e7185a6820bbfe481f857de
Enes koç <32553624+eneskocc@users.noreply.github.com>
2023-02-11 15:38:07 +0300

    Update PdfViewer.js

--- src/Component/PdfViewer.js
+++ src/Component/PdfViewer.js
@@ -1,13 +1,10 @@
 
 import ReactPDF, {
-    Link,
     Text,
-    Font,
     Page,
     View,
     Image,
     Document,
-    StyleSheet,
     PDFViewer,
 } from "@react-pdf/renderer";
 import { educationStyles, expStyles, headerStyles, listStyles, resumeStyles, skilsStyles, titleStyles } from "./PdfViewerStyles";
@@ -92,48 +89,7 @@ const ExperienceEntry = ({ company, details, position, date }) => {
     );
 };
 
-const experienceData = [
-    {
-        company: "Jedi Temple, Coruseant",
-        date: "A long time ago...",
-        details: [
-            "Started a new Jedi Temple in order to train the next generation of Jedi Masters",
-            "Discovered and trained a new generation of Jedi Knights, which he recruited from within the New Republic",
-            "Communicates with decesased Jedi Masters such as Anakin Skywalker, Yoda, Obi-Wan Kenobi in order to learn the secrets of the Jedi Order",
-        ],
-        position: "Head Jedi Master",
-    },
-    {
-        company: "Rebel Alliance",
-        date: "A long time ago...",
-        details: [
-            "Lead legions of troops into battle while demonstrating bravery, competence and honor",
-            "Created complicated battle plans in conjunction with other Rebel leaders in order to ensure the greatest chance of success",
-            "Defeated Darth Vader in single-combat, and convinced him to betray his mentor, the Emperor",
-        ],
-        position: "General",
-    },
-    {
-        company: "Rebel Alliance",
-        date: "A long time ago...",
-        details: [
-            "Destroyed the Death Star by using the force to find its only weakness and delivering a torpedo into the center of the ship",
-            "Commanded of squadron of X-Wings into battle",
-            "Defeated an enemy AT-AT single handedly after his ship was destroyed",
-            "Awarded a medal for valor and bravery in battle for his successful destruction of the Death Star",
-        ],
-        position: "Lieutenant Commander",
-    },
-    {
-        company: "Tatooine Moisture Refinery",
-        date: "A long time ago...",
-        details: [
-            "Replaced damaged power converters",
-            "Performed menial labor thoughout the farm in order to ensure its continued operation",
-        ],
-        position: "Moisture Farmer",
-    },
-];
+
 
 const Experience = ({ experience }) => (
     <View style={expStyles.container}>
