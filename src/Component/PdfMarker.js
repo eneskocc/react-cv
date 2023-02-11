37f70e28df4a662bc385fe83dd1e792668814067
Enes koç <32553624+eneskocc@users.noreply.github.com>
2023-02-11 15:38:25 +0300

    Update PdfMarker.js

--- src/Component/PdfMarker.js
+++ src/Component/PdfMarker.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react'
-import { Card, Button, Form, InputGroup, ListGroup, Pagination, Badge } from 'react-bootstrap'
-import { act } from 'react-dom/test-utils';
+import { Button, Form, Pagination, Badge } from 'react-bootstrap'
 import { connect } from "react-redux";
 import * as $ from "../redux/action";
-import UploadAndDisplayImage from './UploadAndDisplayImage';
+
+
 function PdfMarker(props) {
     const [name, setname] = useState("")
     const [subtitle, setsubtitle] = useState("")
@@ -12,12 +12,13 @@ function PdfMarker(props) {
     const [school, setschool] = useState({})
     const [skill, setSkill] = useState([{ id: 0, yetenek: "" }])
     const [skillNumber, setSkillNumber] = useState([])
-    const [experience, setExperience] = useState([{ id: 0, company: "", position: "", date: "", details: [] }])
+    const [experience, setExperience] = useState([{ id: 0, company: "", position: "", date: "", details: [{ id: 0, info: "" }] }])
     const [experienceNumber, setExperienceNumber] = useState([])
     const [active, setActive] = useState(1)
     const [selectedImage, setSelectedImage] = useState(null);
 
     let items = [];
+
     for (let number = 1; number <= 5; number++) {
         items.push(
             <Pagination.Item key={number} active={number === active} onClick={() => setActive(number)}>
@@ -25,6 +26,7 @@ function PdfMarker(props) {
             </Pagination.Item>,
         );
     }
+
     const updateSkill = (id, yetenek) => {
         const nextskill = skill.map(skill => {
             if (skill.id !== id) {
@@ -41,7 +43,8 @@ function PdfMarker(props) {
 
         setSkill(nextskill);
     }
-    const updateExperience = (id, company, position, date) => {
+
+    const updateExperience = (id, company, position, date, info, i) => {
         const nextexperience = experience.map(experience => {
             if (experience.id !== id) {
 
@@ -52,7 +55,7 @@ function PdfMarker(props) {
                     company: company == "" ? experience.company : company,
                     position: position == "" ? experience.position : position,
                     date: date == "" ? experience.date : date,
-                    details: []
+                    details: updateExperienceDetail(i, experience.details, info)
                 };
             }
         });
@@ -60,7 +63,23 @@ function PdfMarker(props) {
         setExperience(nextexperience);
     }
 
-    console.log(experience)
+    const updateExperienceDetail = (id, details, info) => {
+        const nextExperienceDetail = details.map(item => {
+            if (item.id !== id) {
+
+                return item;
+            } else {
+
+                return {
+                    ...item,
+                    info: info == "" ? item.info : info,
+                };
+            }
+        });
+
+        return nextExperienceDetail;
+    }
+
     const renderSwitch = (param) => {
         switch (param) {
             case 1:
@@ -120,48 +139,78 @@ function PdfMarker(props) {
                         <Form.Label htmlFor="TextInput">Yıl</Form.Label>
                         <Form.Control id="email" placeholder="Yıl" value={experience[0]?.date} onChange={(e) => updateExperience(0, "", "", e.target.value, "")} />
                     </Form.Group>
-                    {experienceNumber.map((item, i) => <>
-                        <hr />
-                        <Form.Group className="mb-3">
-                            <Form.Label htmlFor="TextInput">Şirket</Form.Label>
-                            <Form.Control id="sirket" placeholder="Şirket" value={experience[i + 1]?.company} onChange={(e) => {
-                                if (experience[i + 1]?.company != undefined) {
-                                    updateExperience(i + 1, e.target.value, "", "", "")
-                                } else {
-                                    setExperience([...experience, { id: i + 1, company: e.target.value }])
-                                }
 
-                                //
-                            }} />
-                        </Form.Group>
-                        <Form.Group className="mb-3">
-                            <Form.Label htmlFor="TextInput">Ünvan</Form.Label>
-                            <Form.Control id="subtitle" placeholder="Ünvan" value={experience[i + 1]?.position} onChange={(e) => {
-                                if (experience[i + 1]?.company != undefined) {
-                                    updateExperience(i + 1, "", e.target.value, "", "", "")
-                                } else {
-                                    setExperience([...experience, { id: i + 1, position: e.target.value }])
-                                }
-
-                                //
-                            }} />
-                        </Form.Group>
-                        <Form.Group className="mb-3">
-                            <Form.Label htmlFor="TextInput">Yıl</Form.Label>
-                            <Form.Control id="email" placeholder="Yıl" value={experience[i + 1]?.date} onChange={(e) => {
-                                if (experience[i + 1]?.company != undefined) {
-                                    updateExperience(i + 1, "", "", e.target.value, "")
-                                } else {
-                                    setExperience([...experience, { id: i + 1, date: e.target.value }])
-                                }
-
-                                //
-                            }} />
-                        </Form.Group>
-                    </>)}
-                    <Button variant="danger" onClick={() => {
+                    {
+                        experience[0].details.length > 0 && experience[0].details.map((item, i) => <>
+                            <hr />
+                            <Form.Group className="ml-3 mr-3 p-6">
+                                <Form.Label htmlFor="TextInput">Detay {i + 1}</Form.Label>
+                                <Form.Control id="detay" placeholder="Detay" value={item.info} onChange={(e) => updateExperience(0, "", "", "", e.target.value, i)} />
+                            </Form.Group>
+                        </>
+                        )}
+                    <Button variant="danger" className="mb-3" onClick={() => {
+                        experience[0].details.push({ id: experience[0].details.length, info: "" });
+                    }}>
+                        Add Experience Detail
+                    </Button>
+                    {
+                        experienceNumber.map((item, i) => <>
+                            <hr />
+                            <Form.Group className="mb-3">
+                                <Form.Label htmlFor="TextInput">Şirket</Form.Label>
+                                <Form.Control id="sirket" placeholder="Şirket" value={experience[i + 1]?.company} onChange={(e) => {
+                                    if (experience[i + 1]?.company != undefined) {
+                                        updateExperience(i + 1, e.target.value, "", "", "")
+                                    } else {
+                                        setExperience([...experience, { id: i + 1, company: e.target.value }])
+                                    }
+                                }} />
+                            </Form.Group>
+                            <Form.Group className="mb-3">
+                                <Form.Label htmlFor="TextInput">Ünvan</Form.Label>
+                                <Form.Control id="subtitle" placeholder="Ünvan" value={experience[i + 1]?.position} onChange={(e) => {
+                                    if (experience[i + 1]?.company != undefined) {
+                                        updateExperience(i + 1, "", e.target.value, "", "", "")
+                                    } else {
+                                        setExperience([...experience, { id: i + 1, position: e.target.value }])
+                                    }
+                                }} />
+                            </Form.Group>
+                            <Form.Group className="mb-3">
+                                <Form.Label htmlFor="TextInput">Yıl</Form.Label>
+                                <Form.Control id="email" placeholder="Yıl" value={experience[i + 1]?.date} onChange={(e) => {
+                                    if (experience[i + 1]?.company != undefined) {
+                                        updateExperience(i + 1, "", "", e.target.value, "")
+                                    } else {
+                                        setExperience([...experience, { id: i + 1, date: e.target.value }])
+                                    }
+                                }} />
+                            </Form.Group>
+                            {
+                                experience[i + 1].details.length > 0 && experience[i + 1].details.map((item, i) => <>
+                                    <hr />
+                                    <Form.Group className="mb-3 p-6">
+                                        <Form.Label htmlFor="TextInput">Detay {i + 1}</Form.Label>
+                                        <Form.Control id="detay" placeholder="Detay" value={item.info} onChange={(e) => updateExperience(i + 1, "", "", "", e.target.value, i)} />
+                                    </Form.Group>
+                                </>
+                                )}
+                            <Button variant="danger" onClick={() => {
+                                experience[i + 1].details.push({ id: experience[i + 1].details.length, info: "" });
+                                updateExperience(i + 1, "", "", "", "", experience[i + 1].details.length)
+                            }}>
+                                Add Experience Detail
+                            </Button>
+                        </>
+                        )}
+                    <br />
+                    <Button variant="danger mt-3" onClick={() => {
                         setExperienceNumber([...experienceNumber, { number: "e.target.value" }])
-                    }}>Add Experience</Button>
+                        setExperience([...experience, { id: experienceNumber.length + 1, company: "", position: "", date: "", details: [{ id: 0, info: "" }] }]);
+                    }}>
+                        Add Experience
+                    </Button>
                 </>;
             case 4:
                 return <>
@@ -172,41 +221,44 @@ function PdfMarker(props) {
                         <Form.Label htmlFor="TextInput">Yetenek 1</Form.Label>
                         <Form.Control id={skill[0]?.yetenek} placeholder="Yetenek" value={skill[0]?.yetenek} onChange={(e) => { updateSkill(0, e.target.value) }} />
                     </Form.Group>
-                    {skillNumber.map((item, i) => <>
-                        <Form.Group className="mb-3">
-                            <Form.Label htmlFor="TextInput">Yetenek {i + 2}</Form.Label>
-                            <Form.Control id={skill[i]?.yetenek} placeholder="Yetenek" value={skill[i + 1]?.yetenek} onChange={(e) => {
-                                if (skill[i + 1]?.yetenek != undefined) {
-                                    updateSkill(i + 1, e.target.value)
-                                } else {
-                                    setSkill([...skill, { id: i + 1, yetenek: e.target.value }])
-                                }
+                    {
+                        skillNumber.map((item, i) => <>
+                            <Form.Group className="mb-3">
+                                <Form.Label htmlFor="TextInput">Yetenek {i + 2}</Form.Label>
+                                <Form.Control id={skill[i]?.yetenek} placeholder="Yetenek" value={skill[i + 1]?.yetenek} onChange={(e) => {
+                                    if (skill[i + 1]?.yetenek != undefined) {
+                                        updateSkill(i + 1, e.target.value)
+                                    } else {
+                                        setSkill([...skill, { id: i + 1, yetenek: e.target.value }])
+                                    }
 
-                                //
-                            }} />
-                        </Form.Group>
-                    </>)}
+                                    //
+                                }} />
+                            </Form.Group>
+                        </>
+                        )}
                     <Button variant="danger" onClick={() => {
                         setSkillNumber([...skillNumber, { number: "e.target.value" }])
-                    }}>Add Skill</Button>
+                    }}>
+                        Add Skill
+                    </Button>
                 </>;
             case 5:
                 return <h1>
                     Finish  <Badge bg="danger">5</Badge>
                     <div>
-                        {selectedImage && (
-                            <div>
-                                <img alt="not fount" width={"250px"} src={URL.createObjectURL(selectedImage)} />
-                                <br />
-                                
-                                <Button variant="danger"onClick={() => setSelectedImage(null)}>Remove</Button>
-                            </div>
-                        )}
+                        {
+                            selectedImage && (
+                                <div>
+                                    <img alt="not fount" width={"250px"} src={URL.createObjectURL(selectedImage)} />
+                                    <br />
+                                    <Button variant="danger" onClick={() => setSelectedImage(null)}>Remove</Button>
+                                </div>
+                            )}
                         <input
                             type="file"
                             name="myImage"
                             onChange={(event) => {
-                                console.log(event.target.files[0]);
                                 setSelectedImage(event.target.files[0]);
                             }}
                         />
@@ -221,18 +273,20 @@ function PdfMarker(props) {
     }
 
     return (
-
-
         <Form>
             <fieldset>
                 <br />
-                {renderSwitch(active)}
+                {
+                    renderSwitch(active)
+                }
                 <div>
                     <br />
-                    <Pagination size="lg">{items}</Pagination>
+                    <Pagination size="lg">
+                        {items}
+                    </Pagination>
                     <br />
                 </div>
-                <Button variant="danger" onClick={() => props.addName(name, subtitle, email,phoneNumber, school, skill, experience, selectedImage)}>Save All</Button>
+                <Button variant="danger" onClick={() => props.addName(name, subtitle, email, phoneNumber, school, skill, experience, selectedImage)}>Save All</Button>
             </fieldset>
         </Form>
 
@@ -249,9 +303,10 @@ const mapDispatchToProps = (dispatch, props) => ({
         name,
         subtitle,
         email,
+        phoneNumber,
         school,
         skill,
-        experience, selectedImage
+        experience, selectedImage,
     ) => {
 
         dispatch({
@@ -260,6 +315,7 @@ const mapDispatchToProps = (dispatch, props) => ({
                 name,
                 subtitle,
                 email,
+                phoneNumber,
                 school,
                 skill,
                 experience, selectedImage
