02caf3f7f4f436b9fbfad45493090e3482cbcd5e
Enes koç <32553624+eneskocc@users.noreply.github.com>
2023-02-12 12:45:43 +0300

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,4 +1,4 @@
-import React ,{useEffect}from 'react'
+import React, { useEffect } from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
 import PdfMarker from '../Component/PdfMarker'
 import PdfViewer from '../Component/PdfViewer'
@@ -6,45 +6,49 @@ import { connect } from "react-redux";
 import * as $ from "../redux/action";
 
 function Home(props) {
-    
-    useEffect(() => {
-        console.log("home",props)
-    }, [props.INFO])
-    
-    return (
-        <Container>
-            <Row>
-                <Col xs={5}><PdfMarker /></Col>
-                {props.INFO_LOADER ? <Col xs={7}><PdfViewer INFO={props.INFO}/></Col>:<></>}
-                
-            </Row>
 
-        </Container>
-    )
+  useEffect(() => {
+    console.log("home", props)
+  }, [props.INFO])
+
+  return (
+    <Container>
+      <Row>
+        <Col xs={5}><PdfMarker /></Col>
+        {
+          props.INFO_LOADER ?
+            <Col xs={7}><PdfViewer INFO={props.INFO} /></Col>
+            : <></>
+        }
+
+      </Row>
+
+    </Container>
+  )
 }
 const mapStateToProps = (state, props) => {
-    const { INFO_LOADER, INFO_STATUS, INFO } = state.app;
-    return { INFO_LOADER, INFO_STATUS, INFO };
-  };
-  
-  const mapDispatchToProps = (dispatch, props) => ({
-    insert: (
-      name,
-      subtitle,
-      email,
-    ) => {
-      dispatch({
-        type: $.INFO,
-        payload: {
-            name,
-            subtitle,
-            email,
-          
-        },
-      });
-    },
-    
-  
-  });
-  
-  export default connect(mapStateToProps, mapDispatchToProps)(Home);
+  const { INFO_LOADER, INFO_STATUS, INFO } = state.app;
+  return { INFO_LOADER, INFO_STATUS, INFO };
+};
+
+const mapDispatchToProps = (dispatch, props) => ({
+  insert: (
+    name,
+    subtitle,
+    email,
+  ) => {
+    dispatch({
+      type: $.INFO,
+      payload: {
+        name,
+        subtitle,
+        email,
+
+      },
+    });
+  },
+
+
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
